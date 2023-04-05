90dbe2c0bed7cfd967eef8a72f7e2c817583c8fe
Bradley Griffin <bigriff11@gmail.com>
2023-04-05 11:45:17 -0600

    Add way to delete blogs

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,19 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} />
+      <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === "mario")}
         title={"Mario's Blogs"}
+        handleDelete={handleDelete}
       />
-      <BlogList
-        blogs={blogs.filter((blogs) => blogs.author === "yoshi")}
-        title={"Yoshi's Blogs"}
-      />
-      <button onClick={setBlogs}></button>
     </div>
   );
 };

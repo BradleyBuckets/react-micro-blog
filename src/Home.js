c1491b3942aab0828008f06e8f43b8c41b9ce87f
Bradley Griffin <bigriff11@gmail.com>
2023-04-05 11:11:17 -0600

    Add list of blogs and styling

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  const handleClick = () => {
-    console.log("hello scrubs");
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.key}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+      <button onClick={setBlogs}></button>
     </div>
   );
 };

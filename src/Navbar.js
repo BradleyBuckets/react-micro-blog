5ca3ad0932fa2250b9c9592f28a7a800688c5db5
Bradley Griffin <bigriff11@gmail.com>
2023-04-05 18:10:08 -0600

    Rebrand nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Scrub Blog</h1>
+      <h1>The Daily Scrub</h1>
       <div className="links">
         <Link to={"/"}>Home</Link>
         <Link to={"/create"}>New Blog</Link>

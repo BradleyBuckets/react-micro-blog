6a9a752b14be787a752890ca12dba56fc87b99c9
Bradley Griffin <bigriff11@gmail.com>
2023-04-05 18:00:39 -0600

    Add blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
 
+  const { data: blog, isLoading } = useFetch(
+    "http://localhost:8000/blogs/" + id
+  );
+
   return (
     <div className="blog-details">
-      <h2>Blog Details {id} </h2>
+      {isLoading && <h2>Please hold on, we are loading the data...</h2>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <p>{blog.body}</p>
+        </article>
+      )}
     </div>
   );
 };

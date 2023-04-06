c5712ef995898b042d16e163a49b0203c999ae53
Bradley Griffin <bigriff11@gmail.com>
2023-04-06 09:32:01 -0600

    Add state to track input values

--- src/Create.js
+++ src/Create.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
+  let [author, setAuthor] = useState("mario");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
       <form>
         <label>Blog Title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog Content:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog Author:</label>
-        <select>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        <button onClick={(e) => handleSubmit(e)}>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
   );
